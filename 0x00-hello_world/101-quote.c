51dfcad97c0266b20500651e64378880804b85ed
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-02-16 10:54:32 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,5 @@
-#include <stdio.h>
-/**
- * main -  C program that prints without using printf and puts
- *
- * return: 1
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+{#include <stdio.h>
+
+int main(void) {fwrite (2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
-}
+}}
