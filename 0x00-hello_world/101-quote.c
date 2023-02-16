1f6840c9bcf10fad626014cdfb2fd37401abf95a
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-02-16 13:10:19 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - print a C programming nit using printf and puts
+ * main - entry point
  *
- * Return: 1
+ * Return: Always 0
  */
 int main(void)
 {
-	fwrite("\and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19 ,\n");
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
