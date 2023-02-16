aa8d550609d753c5142b65985678be5f504aa36c
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-02-16 05:03:13 -0800

    first c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts ("\"programming is like building a multilimgual puzzle");
-		return(0);
+	puts("\"programming is like building a multilimgual puzzle");
+		return (0);
 }
