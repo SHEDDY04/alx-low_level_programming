f1da28dbb4bee8711109191e081fce9bb4fc3c01
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-02-17 06:17:05 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - program that prints single digit num
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int s = 0;
+
+	for (; s < 10; s++)
+	{
+		printf("%d", s);
+	}
+	putchar('\n');
+	return (0);
+}
