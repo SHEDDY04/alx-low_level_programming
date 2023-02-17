d4235b1c5e624e9e2c3ae564c8feade0d7a4d29d
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-02-17 06:33:30 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case alphabet in reverse
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int s = 0;
+
+	for (; s < 10; S++)
+	{
+		printf("%d", s);
+	}
+	putchar('\n');
+	return (0);
+}
