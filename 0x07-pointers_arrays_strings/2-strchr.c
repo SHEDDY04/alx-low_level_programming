e69531794ce05742643c59fda152dc324eca7641
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-03-06 05:13:35 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,12 @@ char *_strchr(char *s, char c)
 	while (*s)
 
 	{
-		if (*s != c, c == '\0', s++)
-
+		if (*s != c)
+			s++;
 		else
 			return (s);
 	}
-
+	if (c == '\0')
+		return (s);
 	return (NULL);
 }
