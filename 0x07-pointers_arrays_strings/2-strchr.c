85cd09e85a701ba390633e9581a88a812ff10f5d
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-03-06 05:31:11 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _strchr - locates a char in a string
- * @c: ocurrence of the char
- * @s: the string
+ * _strchr - locates a character in a string
+ *
+ * @c: occurrence of the character
+ *
+ * @s: in the string
+ *
  * Return: a pointer to the first occurrence of the character
  */
 
 char *_strchr(char *s, char c)
 {
 	while (*s)
-
 	{
 		if (*s != c)
 			s++;
