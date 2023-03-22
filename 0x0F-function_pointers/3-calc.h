a24532ba268fd10ce80cbe4d1c014e328b7001c2
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-03-22 10:55:02 -0700

    sheddy

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,9 +1,10 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct op - Struct op
- * @op: the operator
- * @f: the fn associated
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
@@ -11,10 +12,10 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int op_add(int, int);
-int op_sub(int, int);
-int op_mul(int, int);
-int op_div(int, int);
-int op_mod(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 #endif
