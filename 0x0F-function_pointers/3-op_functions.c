7da427e8c433470233014f9bf8a938f00b5ba567
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-03-22 11:00:43 -0700

    sheddy

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,38 +1,75 @@
-#include <stdio.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * op_add - add 2 nums
- * @a: num 1
- * @b: num 2
- * Return: sum of a & b
- */
-int op_add(int a, int b) {return (a + b); }
+  * op_add - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
 /**
- * op_sub - subtract nums
- * @a: num 1
- * @b: num 2
- * Return: a - b
- */
-int op_sub(int a, int b) {return (a - b); }
+  * op_sub - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
 /**
- * op_mul - multiply nums
- * @a: num 1
- * @b: num 2
- * Return: a * b
- */
-int op_mul(int a, int b) {return (a * b); }
+  * op_mul - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
 /**
- * op_div - divide nums
- * @a: num 1
- * @b: num 2
- * Return: a / b
- */
-int op_div(int a, int b) {return (a / b); }
+  * op_div - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
 /**
- * op_mod - get reminder of nums
- * @a: num 1
- * @b: num 2
- * Return: a mod b
- */
-int op_mod(int a, int b) {return (a % b); }
+  * op_mod - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
