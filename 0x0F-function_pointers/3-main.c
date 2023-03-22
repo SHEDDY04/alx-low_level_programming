0b21820de14e75c6b4ceb2d69ef479a39edddb2b
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-03-22 10:52:00 -0700

    sheddy

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,32 @@
-#include <stdio.h>
 #include "3-calc.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - main function
- * @argc: argv counter
- * @argv:  arguments passed to the fn
- * Return: ans
- */
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, ans;
-	int (*ptr)(int, int);
+	int (*oprt)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	if ((*argv[2] == '/' || *argv[2] == '%') && (num1 == 0 || num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	ptr = get_op_func(argv[2]);
-	if (ptr == NULL)
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	ans = ptr(num1, num2);
-	printf("%d\n", ans);
+
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
