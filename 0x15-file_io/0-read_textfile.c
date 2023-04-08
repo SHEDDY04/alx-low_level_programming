ea2a9e3093740b95eb55853792c581b92b1c2c56
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-04-08 10:42:23 -0700

    sheddy

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,38 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * @letters: The number of letters the to print
  *
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t o, r, w;
+	char *buffer;
+
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
-	FILE *file = fopen(filename, "r");
-
-	if (file == NULL)
-	{
-		return (0);
-	}
-
-	char *buffer = (char *) malloc(sizeof(char) * (letters + 1));
-
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		fclose(file);
+		free(buffer);
 		return (0);
 	}
-	ssize_t bytes_read = fread(buffer, sizeof(char), letters, file);
 
-	if (bytes_read <= 0, || fwrite(buffer, sizeof(char), bytes_read, stdout)
-			!= bytes_read)
-		{
-			fclose(file);
-			free(buffer);
-			return (0);
-		}
-		fclose(file);
-		free(buffer);
-		return (bytes_read);
+	free(buffer);
+	close(o);
+
+	return (w);
 }
