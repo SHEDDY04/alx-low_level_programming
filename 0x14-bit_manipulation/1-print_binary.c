3961014289c909fc45eca8de4957edb8e8fd8510
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-04-10 18:14:21 -0700

    sheddy

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to print in binary.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+	size_t bit_count = 0;
+
+	mask = mask << (sizeof(unsigned long int) * 8 - 1);
+
+	while (mask > 0)
+	{
+		if ((n & mask) == mask)
+		{
+			_putchar('1');
+			bit_count++;
+		}
+		else if (bit_count > 0 || mask == 1)
+			_putchar('0');
+
+		mask = mask >> 1;
+	}
+
+	if (bit_count == 0)
+		_putchar('0');
+}
