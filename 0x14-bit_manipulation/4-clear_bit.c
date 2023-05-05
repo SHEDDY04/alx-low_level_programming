0720dd6db60765c160ce909044f12e82ef255d89
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-05-05 14:21:24 -0700

    sheddy

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,16 +8,16 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1;
-	
+
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	
+
 	mask <<= index;
-	
+
 	mask = ~mask;
-	
+
 	*n &= mask;
-	
+
 	return (1);
 }
 
